9376cca39adbdf3d2c850bc1ec9cc19e39d175b7
Yassinean <yassinedubraska@gmail.com>
2024-10-30 13:49:35 +0100

    setup the project spring

---
+++ src/main/java/com/yassine/Gestion_Formation/GestionFormationApplication.java
@@ -0,0 +1,13 @@
+package com.yassine.Gestion_Formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormationApplication.class, args);
+	}
+
+}
